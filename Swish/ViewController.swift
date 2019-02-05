a7118fc4027fcf015660ce7c5007a5e92c6350c9
Cazamere <Cazamere@cazameres-macbook-pro-2.local>
2019-02-04 19:02:40 -0800

    added comments-be sure to check out and edit comments at the top

--- Swish/ViewController.swift
+++ Swish/ViewController.swift
@@ -6,6 +6,18 @@
 //  Copyright © 2019 Cazamere Comrie. All rights reserved.
 //
 
+/*
+Basic notes:
+
+ 1. App is built for use in the "outside", meaning I have yet to initialize a hoop onto a vertical plane. So when hoop is created, it just hovers in the air. Need to add hoop stand and also functionality to place on a vertical wall.
+ 2. Distance has not been factored into the design yet. Need to come up with simple way to determine 2 vs 3 pt (i.e. what is the distance cut off?).
+ 3. Both basketball and backboard look boring, need to find and add textures
+ 4. NEED TO ADD POINT SYSTEM! Just +2 whenever it goes in initially and then add distance for three pointers-accumulate points in the background and pop up +2 or +3 in top right-add two button to same place on Main.storyboard-simple if statement
+ 5. Add timer-just 30 seconds initially to get score and then display final score-then reset to 0-maybe need to add a timer button to the HUD just for now to test all the mechanics
+ 6. Add simple login screen?
+ 
+ */
+
 import UIKit
 import ARKit
 import Each
@@ -19,9 +31,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     var power: Float = 1
     let timer = Each(0.05).seconds
     var basketAdded: Bool = false
-    //var basketAdded: Bool {
-    //    return self.sceneView.scene.rootNode.childNode{nibName;: "ball" recursively: false} != nil
-    //}
     override func viewDidLoad() {
         super.viewDidLoad()
         self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
@@ -41,7 +50,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                 return .continue
             })
         }
-    }
+    } // called when you touch phone-if ball is on scene view, will add power to shot by holding
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         if self.basketAdded == true {
@@ -49,7 +58,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             self.shootBall()
         }
         self.power = 1
-    }
+    } // called when you lift finger off-calls shootBall() to shoot the ball da doiiii
     
     func shootBall() {
         
@@ -60,17 +69,17 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let orientation = SCNVector3(-transform.m31, -transform.m32, -transform.m33)
         let position = location + orientation
         let ball = SCNNode(geometry: SCNSphere(radius: 0.25))
-        ball.geometry?.firstMaterial?.diffuse.contents = UIColor.blue 
+        ball.geometry?.firstMaterial?.diffuse.contents = UIColor.blue // TODO: find texture and add
         ball.position = position
         let body = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(node: ball))
         ball.physicsBody = body
         ball.name = "Basketball"
         body.restitution = 0.2
-        ball.physicsBody?.applyForce(SCNVector3(orientation.x*power, orientation.y*power, orientation.z*power), asImpulse: true)
-        self.sceneView.scene.rootNode.addChildNode(ball)
+        ball.physicsBody?.applyForce(SCNVector3(orientation.x*power, orientation.y*power, orientation.z*power), asImpulse: true) // TODO: change from tap and hold to flick
+        self.sceneView.scene.rootNode.addChildNode(ball) // create another ball after you shoot
         
         
-    }
+    } // create and shoot ball
     
     @objc func handleTap(sender: UITapGestureRecognizer) {
         guard let sceneView = sender.view as? ARSCNView else {return}
@@ -83,7 +92,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     func addBasket(hitTestResult: ARHitTestResult) {
         if basketAdded == false {
-            let basketScene = SCNScene(named: "Ball.scn")
+            let basketScene = SCNScene(named: "Ball.scn") // TODO: create nicer backboard
             let basketNode = basketScene?.rootNode.childNode(withName: "ball", recursively: false)
             let positionOfPlane = hitTestResult.worldTransform.columns.3
             let xPosition = positionOfPlane.x
@@ -96,7 +105,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                 self.basketAdded = true
             }
         }
-    }
+    } // adds backboard and hoop to the scene view
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -111,14 +121,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
             self.planeDetected.isHidden = true
         }
-    }
+    } // just to deal with planeDetected button on top. +2 to indicate button is there for 2 seconds and then disappears
+    
     func removeEveryOtherBall() {
         self.sceneView.scene.rootNode.enumerateChildNodes { (node, _) in
             if node.name == "Basketball" {
                 node.removeFromParentNode()
             }
         }
-    }
+    } // remove the balls yooo
+    
     deinit {
         self.timer.stop()
     }
@@ -128,5 +140,5 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 func +(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
     
     return SCNVector3Make(left.x + right.x, left.y + right.y, left.z + right.z)
-}
+} // useful operator to add 3D vectors
 

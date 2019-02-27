ba6db4f65bb190f589999271b081df879a8cc9fb
Jugal Jain <jjain@ucdavis.edu>
2019-02-27 13:22:53 -0800

    Main Menu Updated

---
+++ Swish/MenuController.swift
@@ -0,0 +1,35 @@
+//
+//  File.swift
+//  Swish
+//
+//  Created by Jugal Jain on 2/27/19.
+//  Copyright © 2019 Cazamere Comrie. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MenuController : UIViewController {
+    
+    @IBOutlet weak var multiplayerStackView: UIStackView!
+    @IBOutlet weak var gameTypeStackView: UIStackView!
+    @IBOutlet weak var backButton: UIButton!
+    
+    override func viewDidLoad() {
+        multiplayerStackView.isHidden = true
+        backButton.isHidden = true
+    }
+    
+    @IBAction func backButtonClicked(_ sender: Any) {
+        self.gameTypeStackView.isHidden = false
+        self.multiplayerStackView.isHidden = true
+        self.backButton.isHidden = true
+    }
+    
+    @IBAction func multiplayerClicked(_ sender: Any) {
+        self.backButton.isHidden = false
+        self.gameTypeStackView.isHidden = true
+        self.multiplayerStackView.isHidden = false
+    }
+}
+
---
+++ Swish/MultipeerSession.swift
@@ -0,0 +1,10 @@
+//
+//  MultipeerConnectivity.swift
+//  Swish
+//
+//  Created by Jugal Jain on 2/27/19.
+//  Copyright © 2019 Cazamere Comrie. All rights reserved.
+//
+
+import MultipeerConnectivity
+

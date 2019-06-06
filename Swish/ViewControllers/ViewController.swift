bbb923cb9da1db4939c27be2ee92acd5b5d3d683
TranDNNguyen <trandnnguyen@gmail.com>
2019-06-06 06:01:01 -0700

    Removed send world map on single player

--- Swish/ViewControllers/ViewController.swift
+++ Swish/ViewControllers/ViewController.swift
@@ -118,6 +118,9 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDele
         Globals.instance.scores.removeAll()
         Globals.instance.scores[Globals.instance.selfPeerID!] = 0
         colorNum = 0
+        if(!Globals.instance.isMulti){
+            sendWorldMapButton.isHidden = true
+        }
     }
     
     // If segueing out of the game, disconnect from the multiplayer session.
